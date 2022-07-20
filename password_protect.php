0007c831aedf800afe1b2bb256a3938af3a9a9e1
chino10030 <107698428+chino10030@users.noreply.github.com>
2022-07-20 12:20:43 -0400

    PHP edit

---
+++ password_protect.php
@@ -0,0 +1,229 @@
+<?php
+
+###############################################################
+# Page Password Protect 2.13
+###############################################################
+# Visit http://www.zubrag.com/scripts/ for updates
+############################################################### 
+#
+# Usage:
+# Set usernames / passwords below between SETTINGS START and SETTINGS END.
+# Open it in browser with "help" parameter to get the code
+# to add to all files being protected. 
+#    Example: password_protect.php?help
+# Include protection string which it gave you into every file that needs to be protected
+#
+# Add following HTML code to your page where you want to have logout link
+# <a href="http://www.example.com/path/to/protected/page.php?logout=1">Logout</a>
+#
+###############################################################
+
+/*
+-------------------------------------------------------------------
+SAMPLE if you only want to request login and password on login form.
+Each row represents different user.
+
+$LOGIN_INFORMATION = array(
+  'zubrag' => 'root',
+  'test' => 'testpass',
+  'admin' => 'admin'
+);
+
+--------------------------------------------------------------------
+SAMPLE if you only want to request only password on login form.
+Note: only passwords are listed
+
+$LOGIN_INFORMATION = array(
+  'root',
+  'testpass',
+  'passwd'
+);
+
+--------------------------------------------------------------------
+*/
+
+##################################################################
+#  SETTINGS START
+##################################################################
+
+// Add login/password pairs below, like described above
+// NOTE: all rows except last must have comma "," at the end of line
+
+$LOGIN_INFORMATION = array(
+  'admin' => 'adminpass',
+   'chino' => 'userpass'
+);
+
+// request login? true - show login and password boxes, false - password box only
+define('USE_USERNAME', true);
+
+// User will be redirected to this page after logout
+define('LOGOUT_URL', 'http://www.example.com/');
+
+// time out after NN minutes of inactivity. Set to 0 to not timeout
+define('TIMEOUT_MINUTES', 0);
+
+// This parameter is only useful when TIMEOUT_MINUTES is not zero
+// true - timeout time from last activity, false - timeout time from login
+define('TIMEOUT_CHECK_ACTIVITY', true);
+
+##################################################################
+#  SETTINGS END
+##################################################################
+
+
+///////////////////////////////////////////////////////
+// do not change code below
+///////////////////////////////////////////////////////
+
+// show usage example
+if(isset($_GET['help'])) {
+  die('Include following code into every page you would like to protect, at the very beginning (first line):<br>&lt;?php include("' . str_replace('\\','\\\\',__FILE__) . '"); ?&gt;');
+}
+
+// timeout in seconds
+$timeout = (TIMEOUT_MINUTES == 0 ? 0 : time() + TIMEOUT_MINUTES * 60);
+
+// logout?
+if(isset($_GET['logout'])) {
+  setcookie("verify", '', $timeout, '/'); // clear password;
+  header('Location: ' . LOGOUT_URL);
+  exit();
+}
+
+if(!function_exists('showLoginPasswordProtect')) {
+
+// show login form
+function showLoginPasswordProtect($error_msg) {
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+
+
+<!--META-->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Entrada al Sistema</title>
+
+<!--STYLESHEETS-->
+<link href="pwsp/css/style.css" rel="stylesheet" type="text/css" />
+
+<!--SCRIPTS-->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
+<!--Slider-in icons-->
+<script type="text/javascript">
+$(document).ready(function() {
+	$(".username").focus(function() {
+		$(".user-icon").css("left","-48px");
+	});
+	$(".username").blur(function() {
+		$(".user-icon").css("left","0px");
+	});
+	
+	$(".password").focus(function() {
+		$(".pass-icon").css("left","-48px");
+	});
+	$(".password").blur(function() {
+		$(".pass-icon").css("left","0px");
+	});
+});
+</script>
+
+</head>
+<body>
+
+<!--WRAPPER-->
+<div id="wrapper">
+
+<!--SLIDE-IN ICONS-->
+  <div class="user-icon"></div>
+  <div class="pass-icon"></div>
+    <!--END SLIDE-IN ICONS-->
+  <!--LOGIN FORM-->
+<form name="form1" class="login-form" method="post">
+
+	<!--HEADER-->
+    <div class="header"><!--TITLE-->
+    <h1><img src="pwsp/img/application_list_64.png" width="50" height="47" />Login</h1>
+    <!--END TITLE-->
+    <!--DESCRIPTION--><span style="color:#F00"><?php echo $error_msg; ?> Sistem Login</span></div>
+    <!--END HEADER-->
+	
+	<!--CONTENT-->
+    <div class="content">
+	<!--USERNAME--><input name="access_login" <?php if (!USE_USERNAME) echo'disabled="disabled"';?> type="text" class="input username" value="Username" onfocus="this.value=''" /><!--END USERNAME-->
+    <!--PASSWORD--><input name="access_password" type="password" class="input password" value="Password" onfocus="this.value=''" /><!--END PASSWORD-->
+    </div>
+    <!--END CONTENT-->
+    
+    <!--FOOTER-->
+    <div class="footer" align="center">
+    <input type="submit" name="submit" value="Login" class="button" />
+    </div>
+    <!--END FOOTER-->
+
+</form>
+<!--END LOGIN FORM-->
+</div>
+<!--END WRAPPER-->
+
+<!--GRADIENT--><div class="gradient"></div><!--END GRADIENT-->
+
+</body>
+</html>
+<?php
+  // stop at this point
+  die();
+}
+}
+
+// user provided password
+if (isset($_POST['access_password'])) {
+
+  $login = isset($_POST['access_login']) ? $_POST['access_login'] : '';
+  $pass = $_POST['access_password'];
+  if (!USE_USERNAME && !in_array($pass, $LOGIN_INFORMATION)
+  || (USE_USERNAME && ( !array_key_exists($login, $LOGIN_INFORMATION) || $LOGIN_INFORMATION[$login] != $pass ) ) 
+  ) {
+    showLoginPasswordProtect("Password Incorrecto.");
+  }
+  else {
+    // set cookie if password was validated
+    setcookie("verify", md5($login.'%'.$pass), $timeout, '/');
+    
+    // Some programs (like Form1 Bilder) check $_POST array to see if parameters passed
+    // So need to clear password protector variables
+    unset($_POST['access_login']);
+    unset($_POST['access_password']);
+    unset($_POST['Submit']);
+  }
+
+}
+
+else {
+
+  // check if password cookie is set
+  if (!isset($_COOKIE['verify'])) {
+    showLoginPasswordProtect("");
+  }
+
+  // check if cookie is good
+  $found = false;
+  foreach($LOGIN_INFORMATION as $key=>$val) {
+    $lp = (USE_USERNAME ? $key : '') .'%'.$val;
+    if ($_COOKIE['verify'] == md5($lp)) {
+      $found = true;
+      // prolong timeout
+      if (TIMEOUT_CHECK_ACTIVITY) {
+        setcookie("verify", md5($lp), $timeout, '/');
+      }
+      break;
+    }
+  }
+  if (!$found) {
+    showLoginPasswordProtect("");
+  }
+
+}
+
+?>
